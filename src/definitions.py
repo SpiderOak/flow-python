8170329710139447db64baf08b7d68c2310270dd
Lucas Manuel Rodriguez <lucas@spideroak.com>
2017-04-11 19:02:43 -0300

    Revert "Update paths for auto-updates"
    This reverts commit cf68d9bb12b9c0398ae102dcdb8e3cf7820ce4ac.

--- src/definitions.py
+++ src/definitions.py
@@ -24,11 +24,10 @@ _CONFIG_OS_PATH_MAP = {
 # Needed for Python 3.5
 _CONFIG_OS_PATH_MAP['linux'] = _CONFIG_OS_PATH_MAP['linux2']
 
-# We add '%s' to support auto-updates new packaging
-_DEFAULT_APP_OSX_PATH = "/Applications/Semaphor.app/%sContents/Resources/app"
-_DEFAULT_APP_LINUX_RPM_PATH = "/opt/Semaphor-linux-x64/%sresources/app"
-_DEFAULT_APP_LINUX_DEB_PATH = "/usr/share/semaphor/%sresources/app"
-_DEFAULT_APP_WINDOWS_PATH = r"Semaphor\%sresources\app"
+_DEFAULT_APP_OSX_PATH = "/Applications/Semaphor.app/Contents/Resources/app"
+_DEFAULT_APP_LINUX_RPM_PATH = "/opt/Semaphor-linux-x64/resources/app"
+_DEFAULT_APP_LINUX_DEB_PATH = "/usr/share/semaphor/resources/app"
+_DEFAULT_APP_WINDOWS_PATH = r"Semaphor\resources\app"
 
 # Default dirs and binaries
 _DEFAULT_ATTACHMENT_DIR = "downloads"
@@ -38,52 +37,26 @@ _DEFAULT_FLOWAPPGLUE_BINARY_DEV_NAME = "flowappglue%s" % _EXE_EXT
 _DEFAULT_FLOWAPPGLUE_BINARY_PROD_NAME = "semaphor-backend%s" % _EXE_EXT
 
 
-def _update_replace(path, update, update_part):
-    """Formats the 'path' with 'update_part' if 'update' is True.
-    If 'update' is False, then the '%s' is replaced by an empty string.
-    """
-    return path % (update_part if update else "")
-
-
-def _osx_app_path(update=False):
+def _osx_app_path():
     """Returns the default application directory for OSX."""
-    return _update_replace(
-        _DEFAULT_APP_OSX_PATH,
-        update,
-        "Contents/Resources/",
-    )
+    return _DEFAULT_APP_OSX_PATH
 
 
-def _linux_app_path(update=False):
+def _linux_app_path():
     """Returns the default application directory for Linux
     depending on the packaging (deb or rpm).
     """
     # check if RPM first
-    rpm_path = _update_replace(
-        _DEFAULT_APP_LINUX_RPM_PATH,
-        update,
-        "resources/",
-    )
-    if os.path.exists(rpm_path):
-        return rpm_path
+    if os.path.exists(_DEFAULT_APP_LINUX_RPM_PATH):
+        return _DEFAULT_APP_LINUX_RPM_PATH
     # otherwise return DEB
-    return _update_replace(
-        _DEFAULT_APP_LINUX_DEB_PATH,
-        update,
-        "resources/",
-    )
+    return _DEFAULT_APP_LINUX_DEB_PATH
 
 
-def _windows_app_path(update=False):
+def _windows_app_path():
     """Returns the default application directory for Windows."""
-    return os.path.join(
-        os.environ["ProgramFiles"],
-        _update_replace(
-            _DEFAULT_APP_WINDOWS_PATH,
-            update,
-            "resources\\",
-        ),
-    )
+    return os.path.join(os.environ["ProgramFiles"],
+                        _DEFAULT_APP_WINDOWS_PATH)
 
 
 def _get_home_directory():
@@ -121,28 +94,12 @@ def get_default_db_path():
     return _get_config_path()
 
 
-def _get_app_path():
-    """Returns the app path depending on the platform.
-    It checks if the auto-updates version of Semaphor is installed.
-    Otherwise, it returns the default app path.
-    """
-    app_path_func = _APP_OS_PATH_MAP[sys.platform]
-    app_path = app_path_func(update=True)
-    if not os.path.exists(app_path):
-        app_path = app_path_func(update=False)
-    return app_path
-
-
 def get_default_schema_path():
     """Returns the default schema directory depending on the platform.
     E.g. on OSX it would be:
     /Applications/Semaphor.app/Contents/Resources/app/schema.
     """
-    app_path = _get_app_path()
-    return os.path.join(
-        app_path,
-        _DEFAULT_SCHEMA_DIR,
-    )
+    return os.path.join(_APP_OS_PATH_MAP[sys.platform](), _DEFAULT_SCHEMA_DIR)
 
 
 def get_default_attachment_path():
@@ -157,15 +114,12 @@ def get_default_flowappglue_path():
     """Returns a string with the absolute path for
     the flowappglue binary; the return value depends on the platform.
     """
-    app_path = _get_app_path()
     flowappglue_path = os.path.join(
-        app_path,
-        _DEFAULT_FLOWAPPGLUE_BINARY_PROD_NAME,
-    )
+        _APP_OS_PATH_MAP[sys.platform](),
+        _DEFAULT_FLOWAPPGLUE_BINARY_PROD_NAME)
     if os.path.isfile(flowappglue_path):
         return flowappglue_path
     flowappglue_path = os.path.join(
-        app_path,
-        _DEFAULT_FLOWAPPGLUE_BINARY_DEV_NAME,
-    )
+        _APP_OS_PATH_MAP[sys.platform](),
+        _DEFAULT_FLOWAPPGLUE_BINARY_DEV_NAME)
     return flowappglue_path
